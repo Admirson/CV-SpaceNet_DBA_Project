2b68ac481a9583853c8f39a9449a89695d52b859
Admirson <151019385+Admirson@users.noreply.github.com>
2026-07-28 18:58:28 -0100

    Conecsão ODBC

---
+++ CV-SpaceNet_AP/conecsao.py
@@ -0,0 +1,12 @@
+import pyodbc
+
+# Conexão usando DSN configurado
+conn = pyodbc.connect("DSN=PostgreSQL35W;UID=postgres;PWD=FzSecret@24")
+
+cursor = conn.cursor()
+cursor.execute("SELECT version();")
+
+for row in cursor.fetchall():
+    print(row)
+
+conn.close()
